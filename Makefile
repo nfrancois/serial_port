ec47a6392aae860384f9ffdc1681ff43dad7e273
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-11-27 16:04:01 +0100

    Test with dummy serial port

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: clean build
+all: clean build test
 
 clean:
 	rm -rf libserial_port.*	
 
-build: 
+build:
 	dart build.dart
+
+test:
+	dart test_serial_port.dart 
--- test_serial_port.dart
+++ test_serial_port.dart
@@ -1,9 +1,22 @@
 library test_serial_port;
 
 import 'serial_port.dart';
+import 'dart:io';
+
+// TODO : test with unnitest
+// TODO : setup & tear down
+
+void check(bool condition, String message) {
+  if (!condition) {
+    throw new StateError(message);
+  }
+}
 
 void main() {
-  var successOpen = open("/dev/tty.usbmodemfd131", 9600);
-  
-  print(successOpen);
+  //var successOpen = open("/dev/tty.usbmodemfd131", 9600);
+  var dummySerialPort = new File("dummySerialPort.tmp")..createSync();
+  var successOpen = open(dummySerialPort.path, 9600);
+  check(successOpen == true, "Should open");
+  var successClose = close();
+  check(successOpen == true, "Should close");
 }
