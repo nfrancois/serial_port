3fcaaf4bdeb722b20724781cf678e8a32b283608
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-11-27 19:59:46 +0100

    Tests with unittest

--- test_serial_port.dart
+++ test_serial_port.dart
@@ -1,5 +1,7 @@
 library test_serial_port;
 
+import 'package:unittest/unittest.dart';
+import 'package:unittest/matcher.dart' as m;
 import 'serial_port.dart';
 import 'dart:io';
 
@@ -13,10 +15,17 @@ void check(bool condition, String message) {
 }
 
 void main() {
-  //var successOpen = open("/dev/tty.usbmodemfd131", 9600);
-  var dummySerialPort = new File("dummySerialPort.tmp")..createSync();
-  var successOpen = open(dummySerialPort.path, 9600);
-  check(successOpen == true, "Should open");
-  var successClose = close();
-  check(successOpen == true, "Should close");
+  var dummySerialPort;
+  group('Serial port', () {
+    setUp(() => dummySerialPort = new File("dummySerialPort.tmp")..createSync());
+    tearDown(() => dummySerialPort.deleteSync());
+    
+    test('Just open and close', () {
+	  var successOpen = open(dummySerialPort.path, 9600);
+	  expect(successOpen, isTrue);
+	  var successClose = close();
+	  expect(successClose, isTrue);
+	});
+  });
+
 }
