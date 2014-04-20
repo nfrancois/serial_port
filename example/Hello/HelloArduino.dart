2c786259d04e687de99a6f93dd6d2e19d82e5701
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2014-04-20 15:26:53 +0200

    change portname

--- example/Hello/HelloArduino.dart
+++ example/Hello/HelloArduino.dart
@@ -1,7 +1,7 @@
 import 'package:serial_port/serial_port.dart';
 
 main(){
-  var arduino = new SerialPort("/dev/tty.usbmodemfd131", baudrate : 9600);
+  var arduino = new SerialPort("/dev/tty.usbmodem1411", baudrate : 9600);
   arduino.open().then((_) {
     print("Ctrl-c to close");
     arduino.onRead.listen(print);
