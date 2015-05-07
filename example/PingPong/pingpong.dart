fcb82f66fa72e52a1bb11b5af984671b57b7947e
Nicolas François <nicolas.franc@gmail.com>
2015-05-07 13:16:17 +0000

    Update samples

--- example/PingPong/pingpong.dart
+++ example/PingPong/pingpong.dart
@@ -15,30 +15,30 @@
 import 'package:serial_port/serial_port.dart';
 import 'dart:async';
 
+// Funny game with Arduino : Send "ping" to "Arduno", and reponse "ping" when Arduino send "pong"
 
 var arduino;
 
-main(){
+main() async {
   var buffer = new StringBuffer();
   arduino = new SerialPort("/dev/tty.usbmodem1421");
-  arduino.onRead.map(BYTES_TO_STRING).listen((word){
-    buffer.write(word);
-    if(buffer.toString().startsWith("pong")){
-      print(buffer.toString());
-      buffer.clear();
-      sendPing();
-    }
-  });
-  arduino.open().then((_) {
-    print("Ctrl-c to close");
-    new Timer(new Duration(seconds: 2), sendPing);
-
-  });
+  arduino.onRead.map(BYTES_TO_STRING).listen(checkPong);
+  await arduino.open();
+  print("Ctrl-c to close");
+  new Timer(new Duration(seconds: 2), sendPing);
 }
 
 
 void sendPing(){
-
   print("ping");
   arduino.writeString("ping");
+}
+
+void checkPong(String word){
+  buffer.write(word);
+  if(buffer.toString().startsWith("pong")){
+    print(buffer.toString());
+    buffer.clear();
+    sendPing();
+  }  
 }
